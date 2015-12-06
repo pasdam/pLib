30230a89eedde6cbf4289dc8d490f567c37878f3
pasdam <pasquale.damiani+git@gmail.com>
2015-12-06 13:07:49 +0100

    Merged Extractor into StringUtils

--- src/com/pasdam/utils/text/StringUtils.java
+++ src/com/pasdam/utils/text/StringUtils.java
@@ -5,11 +5,17 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 /**
- * @author Paco
+ * String utility class
+ * 
+ * @author paco
  * @version 1.0
  */
 public class StringUtils {
 	
+	/**
+	 * @param string
+	 * @return
+	 */
 	public static InputStream stringToInputStream(String string) {
 		try {
 			return new ByteArrayInputStream(string.getBytes("UTF-8"));
@@ -18,5 +24,26 @@ public class StringUtils {
 		}
 		return null;
 	}
-
+	
+	/**
+	 * Extract common substring from input strings, both of same size
+	 * 
+	 * @param str1
+	 *            first string
+	 * @param str2
+	 *            second string
+	 * @return the substring in common
+	 */
+	public static String commonSubstrings(String str1, String str2){
+		if (str1.length() == str2.length()) {
+			StringBuilder builder = new StringBuilder(str1.length());
+			for (int i = 0; i < str1.length(); i++) {
+				if (str1.charAt(i) == str2.charAt(i)) {
+					builder.append(str1.charAt(i));
+				}
+			}
+			return builder.toString();
+		}
+		return null;
+	}
 }
