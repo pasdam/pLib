cc383eeba8508a8581a5e261ad11d0a0c21d1bec
pasdam <pasquale.damiani+git@gmail.com>
2015-12-09 13:09:58 +0100

    Added a utility class for Characters.

---
+++ src/com/pasdam/utils/text/CharUtils.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.pasdam.utils.text;
+
+/**
+ * Class with utility methods for char and {@link Character} types
+ * 
+ * @author paco
+ * @version 0.1
+ */
+public class CharUtils {
+	
+	/**
+	 * Returns true if <i>character</i> is a latin digit, in other words if it
+	 * is between '0' and '9', false otherwise
+	 * 
+	 * @param character
+	 *            character to check
+	 * @return true if character is a latin digit, false otherwise
+	 */
+	public static boolean isLatinDigit(char character) {
+		System.out.println(character);
+		return (character >= '0' && character <= '9');
+	}
+}
